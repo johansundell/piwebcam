edfe8b32c85e20e7f6c6c00e98cbc84a3e84a51c
Johan Sundell <johan@pixpro.net>
2016-04-01 14:48:28 +0200

    testing

--- main.go
+++ main.go
@@ -5,15 +5,14 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("mjpg_streamer", "-o 'output_http.so -w /home/johan/src/mjpg-streamer/mjpg-streamer-experimental/www'", "-i 'input_raspicam.so  -ex -hf -vf night'")
+	/*cmd := exec.Command("mjpg_streamer", "-o 'output_http.so -w /home/johan/src/mjpg-streamer/mjpg-streamer-experimental/www'", "-i 'input_raspicam.so  -ex -hf -vf night'")
 	err := cmd.Start()
 	if err != nil {
 		log.Fatal(err)
-	}
+	}*/
 
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme:   "http",
@@ -21,5 +20,5 @@ func main() {
 		Path:     "/",
 		RawQuery: "action=stream",
 	})
-	http.ListenAndServe(":80", proxy)
+	log.Fatal(http.ListenAndServe(":80", proxy))
 }
