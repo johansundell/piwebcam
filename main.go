a8a899146df5a19878ee717b220666e82f342347
Johan Sundell <johan@pixpro.net>
2016-04-01 13:06:49 +0200

    Added cmd to start webcam

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os/exec"
 )
 
 func main() {
+	cmd := exec.Cmd("mjpg_streamer", "-o 'output_http.so -w /home/johan/src/mjpg-streamer/mjpg-streamer-experimental/www'", "-i 'input_raspicam.so  -ex -hf -vf night'")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme:   "http",
 		Host:     "127.0.0.1:8080",
